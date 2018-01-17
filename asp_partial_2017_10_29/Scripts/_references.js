2bf3acaf3d01384d4712e7de03199ac8a7d53e6d
DotNet <DotNet@DESKTOP-H9993PR>
2018-01-17 11:56:04 +0200

    f

--- asp_partial_2017_10_29/Scripts/_references.js
+++ asp_partial_2017_10_29/Scripts/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="jquery-3.2.1.js" />
-/// <reference path="jquery-3.2.1.intellisense.js.js" />
+/// <reference path="jquery-3.2.1.intellisense.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
--- asp_partial_2017_10_29/Scripts/customValidators.js
+++ asp_partial_2017_10_29/Scripts/customValidators.js
@@ -1,6 +1,11 @@
-﻿$.validator.unobstrusive.adapters.addSingleValue("maxwordssingle", "wordcount")
-$.validator.addMethod("maxwordssingle", function (value,elemnt,maxwords) {
-   $.valHooks
+﻿
+
+
+$.validator.unobstrusive.adapters.addSingleValue("maxwordssingle", "wordcount")
+$.validator.addMethod("maxwordssingle", function (value, element, maxwords) {
+    if (value.split(' ').length > maxwords) {
+        return false;
+    }
 })
 
 
